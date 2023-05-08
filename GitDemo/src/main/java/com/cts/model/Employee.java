2d6b34f500ba69fb8af227267d9287b855b036d7
Vijayalakshmi <vijayalakshmi.v3@cognizant.com>
2023-05-08 14:30:56 +0530

    Gitdemo added

---
+++ GitDemo/src/main/java/com/cts/model/Employee.java
@@ -0,0 +1,20 @@
+package com.cts.model;
+
+public class Employee {
+	
+	private int eno;
+	private String ename;
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+}
---
+++ GitDemo/src/main/java/com/cts/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.cts.service;
+
+import com.cts.model.Employee;
+
+public class EmployeeService {
+	
+	public boolean isValid(Employee e) {
+		//Fill code here
+		return false;
+	}
+
+}
